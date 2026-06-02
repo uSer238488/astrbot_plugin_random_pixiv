24e14a6b5e4b8e26068f864bcfd1eb9b22fb5210
uSer238488 <q13153462368@outlook.com>
2026-06-02 15:52:37 +0800

    完成第一版

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
+import aiohttp
+
+
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import astrbot.api.message_components as Comp 
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+
+@register("astrbot_plugin_random_pixiv", "随机pixiv图片", "随机返回一张pixiv图片(非R18)", "v1.1")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
+        self.url = "https://api.tangdouz.com/zzz/p2.php" 
+
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    @filter.command("来张图")
     async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        """返回一张图片"""
+
+        # 请求返回数据
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                    self.url,
+                    params = {"return": "text"},
+                    timeout = aiohttp.ClientTimeout(total=10),
+                ) as resp:
+                    resp.raise_for_status()
+                    image_url = (await resp.text()).strip()
+        except Exception as e:
+            logger.error(f"获取图片失败: {e}")
+            yield event.plain_result("获取图片失败，稍后再试吧")
+            return
+
+        # 返回消息
+        yield event.chain_result([
+            Comp.At(qq = event.get_sender_id()), 
+            Comp.Image.fromURL(url = image_url) 
+        ]) 
+
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
